cbd674a97faebf1dd68a057540c3a7a958ec8315
Mark Howe <mvahowe@gmail.com>
2025-04-23 13:00:33 +0200

    Toggle create status on new content

--- src/components/NewContent.jsx
+++ src/components/NewContent.jsx
@@ -10,7 +10,8 @@ import {
     Typography
 } from "@mui/material";
 import {Close as CloseIcon} from '@mui/icons-material';
-import {i18nContext, doI18n} from "pithekos-lib";
+import {enqueueSnackbar} from "notistack";
+import {i18nContext, debugContext, postJson, doI18n} from "pithekos-lib";
 
 export default function NewContent({open, setOpen}) {
 
@@ -19,6 +20,7 @@ export default function NewContent({open, setOpen}) {
     };
 
     const {i18nRef} = useContext(i18nContext);
+    const {debugRef} = useContext(debugContext);
     const [contentName, setContentName] = useState("");
     const [contentAbbr, setContentAbbr] = useState("");
     const [contentType, setContentType] = useState("");
@@ -28,8 +30,34 @@ export default function NewContent({open, setOpen}) {
     const [bookTitle, setBookTitle] = useState("");
     const [bookAbbr, setBookAbbr] = useState("");
 
-    const handleCreate = () => {
+    const handleCreate = async () => {
         console.log("CREATE");
+        const payload = {
+            content_name: contentName,
+            content_abbr: contentAbbr,
+            content_type: contentType,
+            content_language_code: contentLanguageCode,
+            add_book: showBookFields,
+            book_code: showBookFields ? bookCode : null,
+            book_title: showBookFields ? bookTitle : null,
+            book_abbr: showBookFields ? bookAbbr : null
+        };
+        const response = await postJson(
+            "/git/new",
+            payload,
+            debugRef.current
+        );
+        if (response.ok) {
+            enqueueSnackbar(
+                doI18n("pages:content:content_created", i18nRef.current),
+                {variant: "success"}
+            );
+        } else {
+            enqueueSnackbar(
+                `${doI18n("pages:content:content_creation_error", i18nRef.current)}: ${response.status}`,
+                {variant: "error"}
+            );
+        }
         setOpen(false);
     };
 
@@ -52,7 +80,26 @@ export default function NewContent({open, setOpen}) {
                     <Typography sx={{ml: 2, flex: 1}} variant="h6" component="div">
                         {doI18n("pages:content:new_content", i18nRef.current)}
                     </Typography>
-                    <Button autoFocus color="inherit" onClick={handleCreate}>
+                    <Button
+                        autoFocus
+                        color="inherit"
+                        disabled={
+                            !(
+                                contentName.trim().length > 0 &&
+                                contentAbbr.trim().length > 0 &&
+                                contentType.trim().length > 0 &&
+                                contentLanguageCode.trim().length > 0 &&
+                                (
+                                    !showBookFields || (
+                                        bookCode.trim().length === 3 &&
+                                        bookTitle.trim().length > 0 &&
+                                        bookAbbr.trim().length > 0
+                                    )
+                                )
+                            )
+                        }
+                        onClick={handleCreate}
+                    >
                         {doI18n("pages:content:create", i18nRef.current)}
                     </Button>
                 </Toolbar>
