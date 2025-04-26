0cd4a834af4e1a10a09ce28e319fa9f713777395
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-04-25 21:33:37 -0500

    book code and versification select menus

--- src/components/NewContent.jsx
+++ src/components/NewContent.jsx
@@ -2,16 +2,21 @@ import {useState, useContext, useEffect} from 'react';
 import {
     AppBar,
     Button, Checkbox,
-    Dialog, FormControlLabel, FormGroup,
+    Dialog, FormControl, FormControlLabel, FormGroup,
     IconButton,
     Stack,
     TextField,
     Toolbar,
-    Typography
+    Typography,
+    Select,
+    MenuItem,
+    InputLabel
 } from "@mui/material";
 import {Close as CloseIcon} from '@mui/icons-material';
 import {enqueueSnackbar} from "notistack";
-import {i18nContext, debugContext, postJson, doI18n} from "pithekos-lib";
+import {i18nContext, debugContext, postJson, doI18n, getAndSetJson} from "pithekos-lib";
+import sx from "./Selection.styles";
+import ListMenuItem from "./ListMenuItem";
 
 export default function NewContent({open, setOpen, }) {
 
@@ -32,6 +37,85 @@ export default function NewContent({open, setOpen, }) {
     const [postCount, setPostCount] = useState(0);
     const [showVersification, setShowVersification] = useState(false);
     const [versification, setVersification] = useState("eng");
+
+    const [versificationCodes, setVersificationCodes] = useState([]);
+
+    useEffect(() =>
+      getAndSetJson({
+        url: "/content-utils/versifications",
+        setter: setVersificationCodes
+      }).then(),
+      []
+    );
+    
+    const bookCodes = [
+      { code: 'GEN', id: '01' },
+      { code: 'EXO', id: '02' },
+      { code: 'LEV', id: '03' },
+      { code: 'NUM', id: '04' },
+      { code: 'DEU', id: '05' },
+      { code: 'JOS', id: '06' },
+      { code: 'JDG', id: '07' },
+      { code: 'RUT', id: '08' },
+      { code: '1SA', id: '09' },
+      { code: '2SA', id: '10' },
+      { code: '1KI', id: '11' },
+      { code: '2KI', id: '12' },
+      { code: '1CH', id: '13' },
+      { code: '2CH', id: '14' },
+      { code: 'EZR', id: '15' },
+      { code: 'NEH', id: '16' },
+      { code: 'EST', id: '17' },
+      { code: 'JOB', id: '18' },
+      { code: 'PSA', id: '19' },
+      { code: 'PRO', id: '20' },
+      { code: 'ECC', id: '21' },
+      { code: 'SNG', id: '22' },
+      { code: 'ISA', id: '23' },
+      { code: 'JER', id: '24' },
+      { code: 'LAM', id: '25' },
+      { code: 'EZK', id: '26' },
+      { code: 'DAN', id: '27' },
+      { code: 'HOS', id: '28' },
+      { code: 'JOL', id: '29' },
+      { code: 'AMO', id: '30' },
+      { code: 'OBA', id: '31' },
+      { code: 'JON', id: '32' },
+      { code: 'MIC', id: '33' },
+      { code: 'NAM', id: '34' },
+      { code: 'HAB', id: '35' },
+      { code: 'ZEP', id: '36' },
+      { code: 'HAG', id: '37' },
+      { code: 'ZEC', id: '38' },
+      { code: 'MAL', id: '39' },
+      { code: 'MAT', id: '40' },
+      { code: 'MRK', id: '41' },
+      { code: 'LUK', id: '42' },
+      { code: 'JHN', id: '43' },
+      { code: 'ACT', id: '44' },
+      { code: 'ROM', id: '45' },
+      { code: '1CO', id: '46' },
+      { code: '2CO', id: '47' },
+      { code: 'GAL', id: '48' },
+      { code: 'EPH', id: '49' },
+      { code: 'PHP', id: '50' },
+      { code: 'COL', id: '51' },
+      { code: '1TH', id: '52' },
+      { code: '2TH', id: '53' },
+      { code: '1TI', id: '54' },
+      { code: '2TI', id: '55' },
+      { code: 'TIT', id: '56' },
+      { code: 'PHM', id: '57' },
+      { code: 'HEB', id: '58' },
+      { code: 'JAS', id: '59' },
+      { code: '1PE', id: '60' },
+      { code: '2PE', id: '61' },
+      { code: '1JN', id: '62' },
+      { code: '2JN', id: '63' },
+      { code: '3JN', id: '64' },
+      { code: 'JUD', id: '65' },
+      { code: 'REV', id: '66' },
+    ]
 
     useEffect(
         () => {
@@ -178,14 +262,32 @@ export default function NewContent({open, setOpen, }) {
                 </FormGroup>
                 {
                     showBookFields && <>
-                        <TextField
-                            id="bookCode"
-                            label={doI18n("pages:content:book_code", i18nRef.current)}
-                            value={bookCode}
-                            onChange={(event) => {
-                                setBookCode(event.target.value);
+                      <FormControl>
+                        <InputLabel id="bookCode-label" htmlFor="bookCode" sx={sx.inputLabel}>
+                          {doI18n("pages:content:book_code", i18nRef.current)}
+                        </InputLabel>
+                        <Select
+                            variant="outlined"
+                            labelId="bookCode-label"
+                            name="bookCode"
+                            inputProps={{
+                                id: "bookCode",
                             }}
-                        />
+                            value={bookCode}
+                            label={doI18n("pages:content:book_code", i18nRef.current)}
+                            onChange={(event) => {
+                              setBookCode(event.target.value);
+                            }}
+                            sx={sx.select}
+                        >
+                            {
+                                bookCodes.map((listItem, n) => <MenuItem key={n} value={listItem.code} dense>
+                                        <ListMenuItem listItem={listItem.code}/>
+                                    </MenuItem>
+                                )
+                            }
+                        </Select>
+                      </FormControl>
                         <TextField
                             id="bookTitle"
                             label={doI18n("pages:content:book_title", i18nRef.current)}
@@ -214,17 +316,33 @@ export default function NewContent({open, setOpen, }) {
                             />
                         </FormGroup>
                         {
-                            showVersification && <>
-                                <TextField
-                                    id="bookVersification"
-                                    label={doI18n("pages:content:versification_scheme", i18nRef.current)}
-                                    value={versification}
-                                    onChange={(event) => {
-                                        setVersification(event.target.value);
+                            showVersification &&
+                              <FormControl>
+                                <InputLabel id="booksVersification-label" htmlFor="booksVersification" sx={sx.inputLabel}>
+                                  {doI18n("pages:content:versification_scheme", i18nRef.current)}
+                                </InputLabel>
+                                <Select
+                                    variant="outlined"
+                                    labelId="booksVersification-label"
+                                    name="booksVersification"
+                                    inputProps={{
+                                        id: "bookVersification",
                                     }}
-                                />
-
-                            </>
+                                    value={versification}
+                                    label={doI18n("pages:content:versification_scheme", i18nRef.current)}
+                                    onChange={(event) => {
+                                      setVersification(event.target.value);
+                                    }}
+                                    sx={sx.select}
+                                >
+                                    {
+                                        versificationCodes.map((listItem, n) => <MenuItem key={n} value={listItem} dense>
+                                                <ListMenuItem listItem={listItem}/>
+                                            </MenuItem>
+                                        )
+                                    }
+                                </Select>
+                              </FormControl>
                         }
                     </>
                 }
