4a8566bad8bc50147a094ac82c31c6e3ff86f905
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-04-26 13:09:00 -0500

    Implement eliaspinero's Export USFM Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import {useState, useEffect, useCallback, useContext} from "react"
-import {Box, Grid2, Fab, Menu, MenuItem, Stack, IconButton, Card, CardActionArea, CardContent} from "@mui/material";
+import {Box, Grid2, Fab, Menu, MenuItem, Stack, IconButton } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import EditOffIcon from '@mui/icons-material/EditOff';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CloseIcon from '@mui/icons-material/Close';
 import {getJson, debugContext, i18nContext, netContext, doI18n, postEmptyJson} from "pithekos-lib";
 import dateFormat from 'dateformat';
 import NewContent from "./components/NewContent";
+import DataOut from "./components/DataOut";
 
 function App() {
 
     const {debugRef} = useContext(debugContext);
     const {i18nRef} = useContext(i18nContext);
     const {enabledRef} = useContext(netContext);
+
+    const [contentMenu, setContentMenu] = useState('table');
+    const [bookNames, setBookNames] = useState([]);
+    const [repoSourcePath, setRepoSourcePath] = useState([]);
 
     const [maxWindowHeight, setMaxWindowHeight] = useState(window.innerHeight - 64);
 
@@ -79,6 +85,14 @@ function App() {
         [newIsOpen]
     );
 
+    const dataOutProps = {
+      bookNames,
+      repoSourcePath,
+    }
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    
     return (
         <Box>
             <Fab
@@ -125,75 +139,109 @@ function App() {
                 open={newIsOpen}
                 setOpen={setNewIsOpen}
             />
-            <Box sx={{p: 1, backgroundColor: "#EEE"}}>
-                <Grid2 container spacing={1} sx={{maxHeight: maxWindowHeight, backgroundColor: "#EEE"}}>
-                    {repos.length === 0 &&
-                        <Grid2 item size={12} sx={{backgroundColor: "#FFF"}}>
-                            {doI18n("pages:content:where_create_content", i18nRef.current)}
-                        </Grid2>
-                    }
-                    {
-                        repos
-                            .map(
-                                ((rep, n) => {
-                                        return <>
-                                            <Grid2 key={`${n}-name`} item size={4} sx={{backgroundColor: "#FFF"}}>
-                                                <Stack>
-                                                    <Box><b>{`${rep.name} (${rep.abbreviation})`}</b></Box>
-                                                    {rep.description !== rep.name &&
-                                                        <Box>{rep.description}</Box>
-                                                    }
-                                                </Stack>
-                                            </Grid2>
-                                            <Grid2 key={`${n}-language`} item size={1} sx={{backgroundColor: "#FFF"}}>
-                                                {rep.language_code}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-books`} item size={1} sx={{backgroundColor: "#FFF"}}>
-                                                {`${rep.bookCodes.length} ${doI18n("pages:content:book_or_books", i18nRef.current)}`}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-flavor`} item size={2} sx={{backgroundColor: "#FFF"}}>
-                                                {rep.flavor}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-source`} item size={2} sx={{backgroundColor: "#FFF"}}>
-                                                {
-                                                    rep.path.startsWith("_local_") ?
-                                                        "Local" :
-                                                        rep.path.split("/").slice(0, 2).join(" ")
-                                                }
-                                            </Grid2>
-                                            <Grid2 key={`${n}-date`} item size={1} sx={{backgroundColor: "#FFF"}}>
-                                                {dateFormat(rep.generated_date, "mmm d yyyy")}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-actions`} item size={1} display="flex"
-                                                   justifyContent="flex-end" alignItems="center"
-                                                   sx={{backgroundColor: "#FFF"}}>
-                                                {
-                                                    rep.path.startsWith("_local_") ?
-                                                        <IconButton
-                                                            onClick={
-                                                                async () => {
-                                                                    await postEmptyJson(`/app-state/current-project/${rep.path}`);
-                                                                    window.location.href = "/clients/local-projects";
-                                                                }
-                                                            }
-                                                        >
-                                                            <EditIcon/>
-                                                        </IconButton> :
-                                                        <IconButton disabled={true}>
-                                                            <EditOffIcon/>
-                                                        </IconButton>
-                                                }
-                                                <IconButton>
-                                                    <MoreVertIcon/>
-                                                </IconButton>
-                                            </Grid2>
-                                        </>
-                                    }
-                                )
-                            )
-                    }
-                </Grid2>
-            </Box>
+            {contentMenu === 'table' &&
+              <Box sx={{p: 1, backgroundColor: "#EEE"}}>
+                  <Grid2 container spacing={1} sx={{maxHeight: maxWindowHeight, backgroundColor: "#EEE"}}>
+                      {repos.length === 0 &&
+                          <Grid2 item size={12} sx={{backgroundColor: "#FFF"}}>
+                              {doI18n("pages:content:where_create_content", i18nRef.current)}
+                          </Grid2>
+                      }
+                      {
+                          repos
+                              .map(
+                                  ((rep, n) => {
+                                          return <>
+                                              <Grid2 key={`${n}-name`} item size={4} sx={{backgroundColor: "#FFF"}}>
+                                                  <Stack>
+                                                      <Box><b>{`${rep.name} (${rep.abbreviation})`}</b></Box>
+                                                      {rep.description !== rep.name &&
+                                                          <Box>{rep.description}</Box>
+                                                      }
+                                                  </Stack>
+                                              </Grid2>
+                                              <Grid2 key={`${n}-language`} item size={1} sx={{backgroundColor: "#FFF"}}>
+                                                  {rep.language_code}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-books`} item size={1} sx={{backgroundColor: "#FFF"}}>
+                                                  {`${rep.bookCodes.length} ${doI18n("pages:content:book_or_books", i18nRef.current)}`}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-flavor`} item size={2} sx={{backgroundColor: "#FFF"}}>
+                                                  {rep.flavor}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-source`} item size={2} sx={{backgroundColor: "#FFF"}}>
+                                                  {
+                                                      rep.path.startsWith("_local_") ?
+                                                          "Local" :
+                                                          rep.path.split("/").slice(0, 2).join(" ")
+                                                  }
+                                              </Grid2>
+                                              <Grid2 key={`${n}-date`} item size={1} sx={{backgroundColor: "#FFF"}}>
+                                                  {dateFormat(rep.generated_date, "mmm d yyyy")}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-actions`} item size={1} display="flex"
+                                                    justifyContent="flex-end" alignItems="center"
+                                                    sx={{backgroundColor: "#FFF"}}>
+                                                  {
+                                                      rep.path.startsWith("_local_") ?
+                                                          <IconButton
+                                                              onClick={
+                                                                  async () => {
+                                                                      await postEmptyJson(`/app-state/current-project/${rep.path}`);
+                                                                      window.location.href = "/clients/local-projects";
+                                                                  }
+                                                              }
+                                                          >
+                                                              <EditIcon/>
+                                                          </IconButton> :
+                                                          <IconButton disabled={true}>
+                                                              <EditOffIcon/>
+                                                          </IconButton>
+                                                  }
+                                                  <IconButton
+                                                    onClick={(event) => {
+                                                      setAnchorEl(event.currentTarget);
+                                                      setBookNames(rep.bookCodes);
+                                                      setRepoSourcePath(rep.path);
+                                                  
+                                                  }}
+                                                  >
+                                                      <MoreVertIcon/>
+                                                  </IconButton>
+                                                  <Menu
+                                                    id="basic-menu"
+                                                    anchorEl={anchorEl}
+                                                    open={open}
+                                                    onClose={() => {
+                                                      setAnchorEl(null);
+                                                    }}
+                                                    slotProps={{ list: {'aria-labelledby': 'basic-button',} }}
+                                                  >
+                                                    <MenuItem onClick={() => {
+                                                      setContentMenu('export');
+                                                    }}>
+                                                      {doI18n("pages:content:export_usfm", i18nRef.current)}
+                                                    </MenuItem>
+                                                  </Menu>
+                                              </Grid2>
+                                          </>
+                                      }
+                                  )
+                              )
+                      }
+                  </Grid2>
+              </Box>
+            }
+            {contentMenu === 'export' &&
+              <>
+                <IconButton
+                  onClick={() => {setContentMenu('table')}}
+                >
+                    <CloseIcon/>
+                </IconButton>
+                <DataOut {...dataOutProps}/>
+              </>
+            }
         </Box>
     );
 }
