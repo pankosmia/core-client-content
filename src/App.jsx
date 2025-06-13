1a83be95e1567fb3c322522b3ad7148649606dde
eliaspinero <eliasrpt14@gmail.com>
2025-06-13 13:08:00 -0400

    Added Mark's data-grid, it's not breaking anymore but still isn't showing the actions column.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import {useState, useEffect, useCallback, useContext} from "react"
 import {Box, Grid2} from "@mui/material";
-import {getJson, debugContext} from "pithekos-lib";
+import {DataGrid} from '@mui/x-data-grid';
+import {getJson, debugContext, i18nContext, doI18n} from "pithekos-lib";
 import FabPlusMenu from "./components/FabPlusMenu";
 import ContentRow from "./components/ContentRow";
+import ContentRowButtonPlusMenu from "./components/ContentRowButtonPlusMenu";
+
 
 function App() {
 
     const {debugRef} = useContext(debugContext);
+    const {i18nRef} = useContext(i18nContext);
     const [repos, setRepos] = useState([]);
     const [newIsOpen, setNewIsOpen] = useState(false);
     const [reposModCount, setReposModCount] = useState(0);
+
 
     /** 
      * header 48px + SpaSpa's top margin of 16px + FabPlusMenu 34px + shadow 7px = fixed position of 105px
@@ -66,8 +71,77 @@ function App() {
         [newIsOpen, reposModCount]
     );
 
+     const columns = [
+        {
+            field: 'name',
+            headerName: doI18n("pages:content:row_name", i18nRef.current),
+            flex: 1
+        },
+        {
+            field: 'language',
+            headerName: doI18n("pages:content:row_language", i18nRef.current),
+            flex: 1
+        },
+        {
+            field: 'nBooks',
+            headerName: doI18n("pages:content:row_nbooks", i18nRef.current),
+            type: "number",
+            flex: 1
+        },
+        {
+            field: 'type',
+            headerName: doI18n("pages:content:row_type", i18nRef.current),
+            flex: 1
+        },
+        {
+            field: 'source',
+            headerName: doI18n("pages:content:row_source", i18nRef.current),
+            flex: 1
+        },
+        {
+            field: 'dateUpdated',
+            headerName: doI18n("pages:content:row_date_updated", i18nRef.current),
+            flex: 1
+        },
+        {
+            field: 'actions',
+            headerName: doI18n("pages:content:row_actions", i18nRef.current),
+            flex: 1,
+            
+            renderCell: () => {repos.map((rep) => {
+                <ContentRowButtonPlusMenu
+                    repoInfo={rep}
+                    reposModCount={reposModCount}
+                    setReposModCount={setReposModCount}
+                />
+            })}
+        }
+    ] 
+
+    const rows = repos.map((rep, n) => {
+        return {
+            id: n,
+            name: `${rep.name.trim()}${rep.description.trim() !== rep.name.trim() ? ": " + rep.description.trim() : ""}`,
+            language: rep.language_code,
+            nBooks: rep.bookCodes.length,
+            type: rep.flavor,
+            source: rep.path.startsWith("_local_") ?
+                doI18n("pages:content:local_org", i18nRef.current) :
+                `${rep.path.split("/")[1]} (${rep.path.split("/")[0]})`,
+            dateUpdated: rep.generated_date,
+        }
+    });
+
     return (
-        <Box>
+        <Box sx={{maxHeight: maxWindowHeight, m: 0}}>
+            <FabPlusMenu newIsOpen={newIsOpen} setNewIsOpen={setNewIsOpen}/>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                sx={{paddingTop: "36px"}}
+            />
+        </Box>
+        /* {/* <Box>
             <Box style={{position: 'fixed', width: '100%'}}>
               <FabPlusMenu newIsOpen={newIsOpen} setNewIsOpen={setNewIsOpen}/>
             </Box>
@@ -95,7 +169,7 @@ function App() {
                     }
                 </Grid2>
             </Box>
-        </Box>
+    //    </Box> *//* } */ 
     );
 }
 
