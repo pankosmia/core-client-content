f7677bd7c18efc7421186b54b9f4a2344ade142a
Mark Howe <mvahowe@gmail.com>
2025-06-17 15:01:00 +0200

    Rework fab button(s)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {useState, useEffect, useCallback, useContext} from "react"
 
-import {Box, IconButton, Typography} from "@mui/material";
+import {Box, Grid2, IconButton, Typography} from "@mui/material";
 import {DataGrid} from '@mui/x-data-grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {getJson, debugContext, i18nContext, doI18n, postEmptyJson} from "pithekos-lib";
 import FabPlusMenu from "./components/FabPlusMenu";
 import ContentRowButtonPlusMenu from "./components/ContentRowButtonPlusMenu";
@@ -18,7 +18,7 @@ function App() {
     const [reposModCount, setReposModCount] = useState(0);
 
 
-    /** 
+    /**
      * header 48px + SpaSpa's top margin of 16px + FabPlusMenu 34px + shadow 7px = fixed position of 105px
      * innerHeight is examined in the 2nd Box, so 105px less it's top margin of 16px = 89
      * bottom margin comes from this component, and SpaSpa's bottom margin of 16px is hidden
@@ -72,13 +72,6 @@ function App() {
         [newIsOpen, reposModCount]
     );
 
-    const theme = createTheme({
-        typography: {
-          fontSize: 18,
-        },
-      });
-
-
     const flavorTypes = {
         texttranslation: "scripture",
         "x-bcvnotes": "parascriptural",
@@ -90,7 +83,7 @@ function App() {
         "x-parallel": "parascriptural"
     };
 
-     const columns = [
+    const columns = [
         {
             field: 'name',
             headerName: <Typography>{doI18n("pages:content:row_name", i18nRef.current)}</Typography>,
@@ -131,28 +124,28 @@ function App() {
                 return <>
                     {
                         params.row.path.startsWith("_local_") && ["textTranslation"].includes(params.row.type) ?
-                    <IconButton
-                        onClick={
-                            async () => {
-                                await postEmptyJson(`/navigation/bcv/${params.row.bookCodes[0]}/1/1`);
-                                await postEmptyJson(`/app-state/current-project/${params.row.path}`);
-                                window.location.href = "/clients/local-projects";
-                            }
-                        }
-                    >
-                        <EditIcon/>
-                    </IconButton> :
-                    <IconButton disabled={true}>
-                        <EditOffIcon/>
-                    </IconButton>
+                            <IconButton
+                                onClick={
+                                    async () => {
+                                        await postEmptyJson(`/navigation/bcv/${params.row.bookCodes[0]}/1/1`);
+                                        await postEmptyJson(`/app-state/current-project/${params.row.path}`);
+                                        window.location.href = "/clients/local-projects";
+                                    }
+                                }
+                            >
+                                <EditIcon/>
+                            </IconButton> :
+                            <IconButton disabled={true}>
+                                <EditOffIcon/>
+                            </IconButton>
                     }
                     <ContentRowButtonPlusMenu
-                            repoInfo={params.row}
-                            reposModCount={reposModCount}
-                            setReposModCount={setReposModCount}
-                        />
-                    </>;
-              }
+                        repoInfo={params.row}
+                        reposModCount={reposModCount}
+                        setReposModCount={setReposModCount}
+                    />
+                </>;
+            }
         }
     ]
 
@@ -172,26 +165,28 @@ function App() {
     });
 
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{height: '100%', width: '100%', maxHeight: maxWindowHeight, m: 0}}>
-                <FabPlusMenu newIsOpen={newIsOpen} setNewIsOpen={setNewIsOpen}/>
-                <DataGrid
-                initialState={{
-                    columns: {
-                        columnVisibilityModel: {
-                            nBooks: false,
-                            source: false,
-                            dateUpdated: false
-                        },
-                    },
-                }}
-                    rows={rows}
-                    columns={columns}
-                    sx={{fontSize: "1rem", paddingTop: "36px"}}
-                />
-            </Box>
+            <Grid2 container sx={{height: '100%', width: '100%', maxHeight: maxWindowHeight, m: 0}}>
+                <Grid2 item size={12}>
+                    <FabPlusMenu newIsOpen={newIsOpen} setNewIsOpen={setNewIsOpen}/>
+                </Grid2>
+                <Grid2 item size={12}>
+                    <DataGrid
+                        initialState={{
+                            columns: {
+                                columnVisibilityModel: {
+                                    nBooks: false,
+                                    source: false,
+                                    dateUpdated: false
+                                },
+                            },
+                        }}
+                        rows={rows}
+                        columns={columns}
+                        sx={{fontSize: "1rem"}}
+                    />
+                </Grid2>
+            </Grid2>
 
-        </ThemeProvider>
     );
 }
 
