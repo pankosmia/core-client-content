369fa76cb06e411722cb0176f8c4de5f191cf1f2
tidic <tidic84@gmail.com>
2025-07-10 17:40:01 +0200

    comments deleted

--- src/components/NewOBSContent.jsx
+++ src/components/NewOBSContent.jsx
@@ -18,7 +18,7 @@ import { i18nContext, debugContext, postJson, doI18n, getAndSetJson, getJson } f
 import sx from "./Selection.styles";
 import ListMenuItem from "./ListMenuItem";
 
-export default function NewOBSContent({ open, closeModal, reposModCount, setReposModCount}) {
+export default function NewOBSContent({ open, closeModal, reposModCount, setReposModCount }) {
     const handleClose = () => {
         closeModal();
     }
@@ -28,70 +28,14 @@ export default function NewOBSContent({ open, closeModal, reposModCount, setRepo
     const [contentAbbr, setContentAbbr] = useState("");
     const [contentType, setContentType] = useState("textStories");
     const [contentLanguageCode, setContentLanguageCode] = useState("und");
-    // const [showBookFields, setShowBookFields] = useState(true);
-    // const [bookCode, setBookCode] = useState("TIT");
-    // const [bookTitle, setBookTitle] = useState("Tit");
-    // const [bookAbbr, setBookAbbr] = useState("Ti");
+
     const [postCount, setPostCount] = useState(0);
-    // const [showVersification, setShowVersification] = useState(true);
-    // const [versification, setVersification] = useState("eng");
-    // const [resourceFormat, setResourceFormat] = useState("tn");
-    // const [resourceFormatLabel, setResourceFormatLabel] = useState()
-    // const [resourceFormatOption, setResourceFormatOption] = useState([])
-    // const [versificationCodes, setVersificationCodes] = useState([]);
-    // const [bookCodes, setBookCodes] = useState([]);
-    // const [protestantOnly, setProtestantOnly] = useState(true);
-
-    // useEffect(() =>
-    //     getAndSetJson({
-    //         url: "/content-utils/versifications",
-    //         setter: setVersificationCodes
-    //     }).then(),
-    //     []
-    // );
-
-    // const fetchFormat = async () => {
-    //     try {
-    //         const resourceFormatresponse = await getJson(
-    //             "/app-resources/tsv/templates.json"
-    //         );
-    //         console.log("recuperation des formats de ressources", resourceFormatresponse.json)
-    //         setResourceFormatOption(Object.keys(resourceFormatresponse.json));
-    //         setResourceFormatLabel(resourceFormatresponse.json)
-    //     } catch (error) {
-    //         console.error("Erreur lors de la récupération des ressources", error);
-    //     }
-    // }
-    // useEffect(() => {
-    //     fetchFormat();
-    // }, []);
-
-    // useEffect(
-    //     () => {
-    //         const doFetch = async () => {
-    //             const versificationResponse = await getJson("/content-utils/versification/eng", debugRef.current);
-    //             if (versificationResponse.ok) {
-    //                 setBookCodes(Object.keys(versificationResponse.json.maxVerses));
-    //             }
-    //         };
-    //         if (bookCodes.length === 0) {
-    //             doFetch().then();
-    //         }
-    //     }
-    // );
 
     useEffect(
         () => {
             setContentName("");
             setContentAbbr("");
             setContentLanguageCode("und");
-            // setBookCode("TIT");
-            // setBookTitle("Titus");
-            // setBookAbbr("Ti");
-            // setShowBookFields(true);
-            // setShowVersification(true);
-            // setVersification("eng");
-            // setResourceFormat("tn")
         },
         [postCount]
     );
@@ -152,14 +96,6 @@ export default function NewOBSContent({ open, closeModal, reposModCount, setRepo
                                 contentAbbr.trim().length > 0 &&
                                 contentType.trim().length > 0 &&
                                 contentLanguageCode.trim().length > 0
-                                // versification.trim().length === 3 &&
-                                // (
-                                //     !showBookFields || (
-                                //         bookCode.trim().length === 3 &&
-                                //         bookTitle.trim().length > 0 &&
-                                //         bookAbbr.trim().length > 0
-                                //     )
-                                // )
                             )
                         }
                         onClick={handleCreate}
@@ -203,151 +139,6 @@ export default function NewOBSContent({ open, closeModal, reposModCount, setRepo
                         setContentLanguageCode(event.target.value);
                     }}
                 />
-                {/* <FormControl>
-                    <InputLabel id="resourceFormat-label" htmlFor="resourceFormat"
-                        sx={sx.inputLabel}>
-                        {doI18n("pages:content:resource_format", i18nRef.current)}
-                    </InputLabel>
-                    <Select
-                        variant="outlined"
-                        labelId="resourceFormat-label"
-                        name="resourceFormat"
-                        inputProps={{
-                            id: "resourceFormat",
-                        }}
-                        value={resourceFormat}
-                        label={doI18n("pages:content:resource_format", i18nRef.current)}
-                        onChange={(event) => {
-                            setResourceFormat(event.target.value);
-                        }}
-                        sx={sx.select}
-                    >
-                        {
-                            resourceFormatOption.map((listItem, n) => <MenuItem key={n} value={listItem}
-                                dense>
-                                <ListMenuItem
-                                    listItem={`${listItem} - ${resourceFormatLabel[listItem]?.label}`}
-                                />
-                            </MenuItem>
-                            )
-                        }
-                    </Select>
-                </FormControl> */}
-
-                {/* <FormControl>
-                    <InputLabel id="booksVersification-label" htmlFor="booksVersification"
-                        sx={sx.inputLabel}>
-                        {doI18n("pages:content:versification_scheme", i18nRef.current)}
-                    </InputLabel>
-                    <Select
-                        variant="outlined"
-                        labelId="booksVersification-label"
-                        name="booksVersification"
-                        inputProps={{
-                            id: "bookVersification",
-                        }}
-                        value={versification}
-                        label={doI18n("pages:content:versification_scheme", i18nRef.current)}
-                        onChange={(event) => {
-                            setVersification(event.target.value);
-                        }}
-                        sx={sx.select}
-                    >
-                        {
-                            versificationCodes.map((listItem, n) => <MenuItem key={n} value={listItem}
-                                dense>
-                                <ListMenuItem
-                                    listItem={`${listItem.toUpperCase()} - ${doI18n(`scripture:versifications:${listItem}`, i18nRef.current)}`}
-                                />
-                            </MenuItem>
-                            )
-                        }
-                    </Select>
-                </FormControl> */}
-                {/* <FormGroup>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={showBookFields}
-                                onChange={() => setShowBookFields(!showBookFields)}
-                            />
-                        }
-                        label={doI18n("pages:content:add_book_checkbox", i18nRef.current)}
-                    />
-                </FormGroup> */}
-                {/* {
-                    showBookFields && <>
-                        <Grid2 container spacing={2} justifyItems="flex-end" alignItems="stretch">
-                            <Grid2 item size={2}>
-                                <FormControl sx={{ width: "100%" }}>
-                                    <InputLabel id="bookCode-label" htmlFor="bookCode" sx={sx.inputLabel}>
-                                        {doI18n("pages:content:book_code", i18nRef.current)}
-                                    </InputLabel>
-                                    <Select
-                                        variant="outlined"
-                                        labelId="bookCode-label"
-                                        name="bookCode"
-                                        inputProps={{
-                                            id: "bookCode",
-                                        }}
-                                        value={bookCode}
-                                        label={doI18n("pages:content:book_code", i18nRef.current)}
-                                        onChange={(event) => {
-                                            setBookCode(event.target.value);
-                                            setBookAbbr(
-                                                ["1", "2", "3"].includes(event.target.value[0]) ?
-                                                    event.target.value.slice(0, 2) + event.target.value[2].toLowerCase() :
-                                                    event.target.value[0] + event.target.value.slice(1).toLowerCase()
-                                            );
-                                            setBookTitle(doI18n(`scripture:books:${event.target.value}`, i18nRef.current))
-                                        }}
-                                        sx={sx.select}
-                                    >
-                                        {
-                                            (protestantOnly ? bookCodes.slice(0, 66) : bookCodes).map((listItem, n) => <MenuItem key={n} value={listItem} dense>
-                                                <ListMenuItem listItem={`${listItem} - ${doI18n(`scripture:books:${listItem}`, i18nRef.current)}`} />
-                                            </MenuItem>
-                                            )
-                                        }
-                                    </Select>
-                                </FormControl>
-                                <FormGroup>
-                                    <FormControlLabel
-                                        control={
-                                            <Checkbox
-                                                checked={protestantOnly}
-                                                onChange={() => setProtestantOnly(!protestantOnly)}
-                                            />
-                                        }
-                                        label={doI18n("pages:content:protestant_books_only", i18nRef.current)}
-                                    />
-                                </FormGroup>
-                            </Grid2>
-                            <Grid2 item size={2}>
-                                <TextField
-                                    id="bookAbbr"
-                                    sx={{ width: "100%" }}
-                                    label={doI18n("pages:content:book_abbr", i18nRef.current)}
-                                    value={bookAbbr}
-                                    onChange={(event) => {
-                                        setBookAbbr(event.target.value);
-                                    }}
-                                />
-                            </Grid2>
-                            <Grid2 item size={8}>
-                                <TextField
-                                    id="bookTitle"
-                                    sx={{ width: "100%" }}
-                                    label={doI18n("pages:content:book_title", i18nRef.current)}
-                                    value={bookTitle}
-                                    onChange={(event) => {
-                                        setBookTitle(event.target.value);
-                                    }}
-                                />
-                            </Grid2>
-                        </Grid2>
-                    </>
-                } */}
             </Stack>
         </Dialog>
     );
