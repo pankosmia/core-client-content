679802822ce5bbc5c1b97f3a77a16a129d565cda
lmeline <loise13@hotmail.fr>
2025-08-11 17:12:20 +0200

    modification des dialogs selon les maquettes + merge de la branche de elias

--- src/App.jsx
+++ src/App.jsx
@@ -93,47 +93,36 @@ function App() {
                                         {doI18n("pages:content:quarantined_content", i18nRef.current)}
                                     </MenuItem>
                                 </Menu>
-                                <Modal
-                                    //maxWidth={"lg"}
+                                <Dialog
+                                    fullWidth={true}
+                                    maxWidth={"lg"}
                                     open={experimentDialogOpen}
                                     onClose={handleExperimentDialogClose}
                                 >
-                                    <Box sx={{
-                                        position: 'absolute',
-                                        top: '50%',
-                                        left: '50%',
-                                        transform: 'translate(-50%, -50%)',
-                                        bgcolor: 'background.paper',
-                                        boxShadow: 24,
-                                        borderRadius: 2,
-                                        minWidth:"50vw"
-                                    }}>
-                                        <AppBar  color='secondary' sx={{ position: 'relative',borderTopLeftRadius:4, borderTopRightRadius:4 }}>
-                                            <Toolbar>
-                                                <IconButton
-                                                    edge="start"
-                                                    color="inherit"
-                                                    onClick={() => { handleExperimentDialogClose() }}
-                                                    aria-label={doI18n("pages:content:close", i18nRef.current)}
-                                                >
-                                                    <CloseIcon />
-                                                </IconButton>
-                                                <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                                                    {doI18n("pages:content:experiment_content", i18nRef.current)}
-                                                </Typography>
-                                            </Toolbar>
-                                        </AppBar>
-                                        <Stack>
-                                            <DataGridComponent
-                                                reposModCount={reposModCount}
-                                                setReposModCount={setReposModCount}
-                                                isContentExperiment={true}
-                                                contentUrl={contentUrl}
-                                            />
-                                        </Stack>
-                                    </Box>
-
-                                </Modal>
+                                    <AppBar color='secondary' sx={{ position: 'relative', borderTopLeftRadius: 4, borderTopRightRadius: 4 }}>
+                                        <Toolbar>
+                                            <Typography variant="h6" component="div">
+                                                {doI18n("pages:content:experiment_content", i18nRef.current)}
+                                            </Typography>
+                                        </Toolbar>
+                                    </AppBar>
+                                    <DialogContent>
+                                        <DataGridComponent
+                                            reposModCount={reposModCount}
+                                            setReposModCount={setReposModCount}
+                                            isContentExperiment={true}
+                                            contentUrl={contentUrl}
+                                        />
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button
+                                            onClick={() => { handleExperimentDialogClose() }}
+                                            color="primary"
+                                        >
+                                            {doI18n("pages:content:close", i18nRef.current)}
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
                             </Grid2>
                         </Grid2>
                     </Grid2>
