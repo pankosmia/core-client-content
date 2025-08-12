90141ca14a92b819e52634267fa7ed27deb8810b
eliaspinero <eliasrpt14@gmail.com>
2025-08-12 14:49:23 -0400

    Working endpoints and i18n. #56

--- src/components/RemoteContent.jsx
+++ src/components/RemoteContent.jsx
@@ -10,45 +10,60 @@ import {
     TextField,
     Stack
 } from "@mui/material";
-import {debugContext, i18nContext, doI18n, postEmptyJson} from "pithekos-lib";
+import {debugContext, i18nContext, doI18n, postEmptyJson, getJson} from "pithekos-lib";
 import {enqueueSnackbar} from "notistack";
 
 function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount}) {
     const {i18nRef} = useContext(i18nContext);
-    //const {debugRef} = useContext(debugContext);
-    const [remoteOriginValue, setRemoteOriginValue] = useState('');
-    const [remoteOriginIsValid, setRemoteOriginIsValid] = useState(true);
-    const remoteOriginRegex = new RegExp("[a-zA-Z0-9/\-_]");
+    const {debugRef} = useContext(debugContext);
+    const [remoteNameValue, setRemoteNameValue] = useState('');
+    const [remoteNameIsValid, setRemoteNameIsValid] = useState(true);
+    const remoteNameRegex = new RegExp("^[A-Za-z0-9_-]+$");
 
     const [remoteUrlValue, setRemoteUrlValue] = useState('');
     const [remoteUrlIsValid, setRemoteUrlIsValid] = useState(true);
-    const remoteUrlRegex = new RegExp("/^\S+@\S+:@\S+$/");
-    
-    // BUENO LA UI PARECE ESTAR FUNCIONANDO FINO PERO EL REGEX COMO QUE NO SIRVE. Y NO SE COMO ES ESO DEL JSON.
+    const remoteUrlRegex = new RegExp("^[A-Za-z0-9_:.@/-]+$");
 
-    /* 
-    const copyRepo = async repo_path => {
+    const addRemoteRepo = async repo_path => {
 
-        const copyUrl = `/git/copy/${repo_path}?target_path=_local_/_local_/${repo_path.split("/")[2]}`;
-        const copyResponse = await postEmptyJson(copyUrl, debugRef.current);
-        if (copyResponse.ok) {
+        const remoteListUrl = `/git/remotes/${repo_path}`;
+        const remoteList = await getJson(remoteListUrl, debugRef.current);
+
+        console.log(remoteList);
+        console.log(remoteList.json.payload.remotes.filter((p) => p.name === "origin").length > 0);
+
+        if (remoteList.json.is_good){
+            if (remoteList.json.payload.remotes.filter((p) => p.name === "origin").length > 0) {
+                const deleteUrl = `/git/remote/delete/${repo_path}?remote_name=origin`;
+                const deleteResponse = await postEmptyJson(deleteUrl, debugRef.current);
+                if (deleteResponse.ok) {
+                    console.log("se borró correctamente")
+                } else {
+                    console.error("no se pudo borrar")
+                }
+            }
+        }
+
+        const addUrl = `/git/remote/add/${repo_path}?remote_name=origin&remote_url=${remoteUrlValue}`;
+        const addResponse = await postEmptyJson(addUrl, debugRef.current);
+        if (addResponse.ok) {
             enqueueSnackbar(
-                doI18n("pages:content:repo_copied", i18nRef.current),
+                doI18n("pages:content:remote_repo_added", i18nRef.current),
                 {variant: "success"}
             );
-            setReposModCount(reposModCount + 1)
+            setReposModCount(reposModCount + 1);
+            console.log(addResponse.json)
         } else {
             enqueueSnackbar(
-                doI18n("pages:content:could_not_copy_repo", i18nRef.current),
+                doI18n("pages:content:could_not_add_remote_repo", i18nRef.current),
                 {variant: "error"}
             );
         }
     }
-    */
 
-    const handleRemoteOriginValidation = (e) => {
-        setRemoteOriginIsValid(remoteOriginRegex.test(e.target.value));
-        setRemoteOriginValue(e.target.value);
+    const handleRemoteNameValidation = (e) => {
+        setRemoteNameIsValid(remoteNameRegex.test(e.target.value));
+        setRemoteNameValue(e.target.value);
       };
 
     const handleRemoteUrlValidation = (e) => {
@@ -73,12 +88,12 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
                 </Typography>
                 <Stack spacing={2} sx={{ m: 2 }}>
                     <TextField
-                        id="remote-origin"
-                        label={doI18n("pages:content:remote_origin", i18nRef.current)}
-                        value={remoteOriginValue}
+                        id="remote-name"
+                        label={doI18n("pages:content:remote_name", i18nRef.current)}
+                        value={remoteNameValue}
                         variant="outlined"
-                        onChange={(e) => handleRemoteOriginValidation(e)}
-                        error={!remoteOriginIsValid}
+                        onChange={(e) => handleRemoteNameValidation(e)}
+                        error={!remoteNameIsValid}
                         required={true}
                     />
                     <TextField
@@ -102,17 +117,14 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
             </Button>
             <Button
                 color="warning"
-                onClick={/* async () => {
-                    await copyRepo(repoInfo.path);
-                    closeFn();
-                } */() => {
-                        (remoteOriginIsValid && remoteUrlIsValid) 
-                        ? console.log(remoteOriginValue, remoteUrlValue) 
-                        : console.error("Valores incorrectos");
-                        closeFn()
-                    }
-                }
-            >{doI18n("pages:content:accept", i18nRef.current)}</Button>
+                disabled={!remoteNameIsValid || !remoteUrlIsValid}
+                onClick={async () => {
+                            await addRemoteRepo(repoInfo.path);
+                            closeFn()
+                        }}
+            >
+                {doI18n("pages:content:accept", i18nRef.current)}
+            </Button>
         </DialogActions>
     </Dialog>;
 }
