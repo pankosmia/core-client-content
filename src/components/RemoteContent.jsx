56e2e056b9515805630de48e029427700b011516
eliaspinero <eliasrpt14@gmail.com>
2025-08-13 14:56:15 -0400

    Finished last touches and styling.

--- src/components/RemoteContent.jsx
+++ src/components/RemoteContent.jsx
@@ -17,16 +17,8 @@ import {enqueueSnackbar} from "notistack";
 function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount}) {
     const {i18nRef} = useContext(i18nContext);
     const {debugRef} = useContext(debugContext);
-    const [remoteNameValue, setRemoteNameValue] = useState('origin');
-    //const [remoteNameIsValid, setRemoteNameIsValid] = useState(true);
-    const remoteNameRegex = new RegExp("^[A-Za-z0-9_-]+$");
-
     const [remoteUrlValue, setRemoteUrlValue] = useState('');
-    //const [remoteUrlIsValid, setRemoteUrlIsValid] = useState(true);
     const remoteUrlRegex = new RegExp(/^\S+@\S+:\S+$/);
-
-    //const [valueIsRepeated, setValueIsRepeated] = useState(false);
-
     const [remotes, setRemotes] = useState(null);
 
     useEffect(() => {
@@ -35,6 +27,10 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
             const remoteList = await getJson(remoteListUrl, debugRef.current);
             if (remoteList.ok) {
                 setRemotes(remoteList.json.payload.remotes);
+                const originRecord = remoteList.json.payload.remotes.filter((p) => p.name === "origin")[0];
+                if (originRecord) {
+                    setRemoteUrlValue(originRecord.url)
+                }
             } else {
                 enqueueSnackbar(
                     doI18n("pages:content:could_not_list_remotes", i18nRef.current),
@@ -44,19 +40,9 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
         }
         doFetch().then()
     },
-    [])
+    [reposModCount])
 
     const addRemoteRepo = async repo_path => {
-
- /*        const remoteListUrl = `/git/remotes/${repo_path}`;
-        const remoteList = await getJson(remoteListUrl, debugRef.current);
-        originRecord = remoteList.json.payload.remotes.filter((p) => p.name === "origin")[0];
- */
-        //console.log(originRecord);
-
-        /* if (originRecord.url === remoteUrlValue){
-            setValueIsRepeated(true);
-        } */
 
         if (remotes.filter((p) => p.name === "origin")[0]) {
             const deleteUrl = `/git/remote/delete/${repo_path}?remote_name=origin`;
@@ -77,7 +63,6 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
                 {variant: "success"}
             );
             setReposModCount(reposModCount + 1);
-            //console.log(addResponse.json)
         } else {
             enqueueSnackbar(
                 doI18n("pages:content:could_not_add_remote_repo", i18nRef.current),
@@ -86,17 +71,12 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
         }
     }
 
-    const handleRemoteNameValidation = (e) => {
-        //setRemoteNameIsValid(remoteNameRegex.test(e.target.value));
-        setRemoteNameValue(e.target.value);
-    };
-
     const handleRemoteUrlValidation = (e) => {
-        //setRemoteUrlIsValid(remoteUrlRegex.test(e.target.value));
         setRemoteUrlValue(e.target.value);
     };
 
     return <Dialog
+        fullWidth={true}
         open={open}
         onClose={closeFn}
         slotProps={{
@@ -122,15 +102,6 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
                 </Typography>
                 <Stack spacing={2} sx={{ m: 2 }}>
                     <TextField
-                        id="remote-name"
-                        label={doI18n("pages:content:remote_name", i18nRef.current)}
-                        value={remoteNameValue}
-                        variant="outlined"
-                        onChange={(e) => handleRemoteNameValidation(e)}
-                        error={!remoteNameRegex.test(remoteNameValue)}
-                        required={true}
-                    />
-                    <TextField
                         id="repo-url"
                         label={doI18n("pages:content:remote_repo_url", i18nRef.current)}
                         value={remoteUrlValue}
@@ -146,12 +117,14 @@ function RemoteContent({repoInfo, open, closeFn, reposModCount, setReposModCount
             </DialogContentText>
         </DialogContent>
         <DialogActions>
-            <Button onClick={closeFn}>
+            <Button onClick={closeFn} color='primary'>
                 {doI18n("pages:content:cancel", i18nRef.current)}
             </Button>
             <Button
-                color="warning"
-                disabled={remotes === null || !remoteNameRegex.test(remoteNameValue) || !remoteUrlRegex.test(remoteUrlValue) || (remotes.filter((p) => p.name === "origin")[0]?.url === remoteUrlValue)}
+                autoFocus
+                variant='contained'
+                color="primary"
+                disabled={remotes === null || !remoteUrlRegex.test(remoteUrlValue) || (remotes.filter((p) => p.name === "origin")[0]?.url === remoteUrlValue)}
                 onClick={async () => {
                             await addRemoteRepo(repoInfo.path);
                             closeFn()
