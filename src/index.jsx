5b9a3a221aea2646752e2da202cd415e30429b62
lmeline <loise13@hotmail.fr>
2026-03-06 15:56:55 +0100

    add header

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { SpSpa } from 'pankosmia-rcl';
+import { SpSpa,fallbackTheme } from 'pankosmia-rcl';
 import App from './App';
 import './index.css';
 import { getAndSetJson } from 'pithekos-lib';
 import { ThemeProvider } from '@emotion/react';
 import { useEffect, useState } from 'react';
 import { createTheme } from '@mui/material';
+import { MaterialDesignContent, SnackbarProvider } from 'notistack';
+import styled from '@emotion/styled';
 function AppLayout() {
 
-  const [themeSpec, setThemeSpec] = useState({
-    palette: {
-      primary: {
-        main: "#666",
-      },
-      secondary: {
-        main: "#888",
-      },
-    },
-  });
+  const [themeSpec, setThemeSpec] = useState(fallbackTheme);
 
   useEffect(() => {
     if (
@@ -34,10 +27,39 @@ function AppLayout() {
   }, []);
 
   const theme = createTheme(themeSpec);
+  const CustomSnackbarContent = styled(MaterialDesignContent)(() => ({
+    "&.notistack-MuiContent-error": {
+      backgroundColor: "#FDEDED",
+      color: "#D32F2F",
+    },
+    "&.notistack-MuiContent-info": {
+      backgroundColor: "#E5F6FD",
+      color: "#0288D1",
+    },
+    "&.notistack-MuiContent-warning": {
+      backgroundColor: "#FFF4E5",
+      color: "#EF6C00",
+    },
+    "&.notistack-MuiContent-success": {
+      backgroundColor: "#EDF7ED",
+      color: "#2E7D32",
+    },
+  }));
   return <ThemeProvider theme={theme}>
-    <SpSpa requireNet={false} titleKey="pages:content:title" currentId="content">
-      <App />
-    </SpSpa>
+    <SnackbarProvider
+      Components={{
+        error: CustomSnackbarContent,
+        info: CustomSnackbarContent,
+        warning: CustomSnackbarContent,
+        success: CustomSnackbarContent,
+      }}
+      maxSnack={6}
+    >
+      <SpSpa requireNet={false} titleKey="pages:content:title" currentId="content">
+        <App />
+      </SpSpa>
+    </SnackbarProvider>
+
   </ThemeProvider>
 }
 createRoot(document.getElementById('root'))
