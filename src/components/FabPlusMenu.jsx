5c80df7f96be5d597c9f51a507d8382f02de7c97
eliaspinero <eliasrpt14@gmail.com>
2026-09-15 01:29:18 -0400

    Ok for real now.

--- src/components/FabPlusMenu.jsx
+++ src/components/FabPlusMenu.jsx
@@ -49,7 +49,7 @@ function FabPlusMenu({ clientInterfaces, reposModCount, setReposModCount }) {
 
   return (
     <>
-      <Box sx={{ my: 2 }}>
+      <Box>
         <Fab
           variant="extended"
           color="primary"
